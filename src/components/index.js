f71d83db1f1ea027c3d9cf3882607fa03df74658
Srikanth <srikanthadepu.17@gmail.com>
2023-12-20 23:35:47 +0530

    intial commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeals from "./CardDeals";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testinominals from "./Testinominals";
+
+export {
+  Navbar,
+  Billing,
+  CardDeals,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testinominals,
+  Hero,
+  FeedbackCard,
+  GetStarted,
+  Button,
+};
